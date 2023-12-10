2641d647b170c6b38e1e4e44eb730ba007f9fa2d
akiko23 <usksoaoko@gmail.com>
2023-12-10 16:51:07 +0300

    [add] add initial test for client

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,5 +1,13 @@
 import os
 
+from fastapi.testclient import TestClient
+
 
 def test_db_exits(db_path: str) -> None:
     assert os.path.exists(db_path)
+
+
+def test_client(client: TestClient):
+    resp = client.get('/')
+    assert resp.status_code == 200
+    assert resp.json() == {"msg": "Welcome to FastAPI-blog!"}
